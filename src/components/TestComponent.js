9e88ddf51ca2029ad11b458e100659b1201a4cfe
Yujŏnja <hardboiled65@gmail.com>
2021-11-11 10:42:05 +0900

    Remove and add to gitignore compiled js files.

--- src/components/TestComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './TestComponent.scss';
-const TestComponent = () => {
-    const hello = 'hello!';
-    return (React.createElement("div", { className: "TestComponent" }, hello));
-};
-export default TestComponent;
