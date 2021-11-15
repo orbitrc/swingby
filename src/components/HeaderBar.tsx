3e07b8f951a6bfe4cde65847752442e48b552f02
Yujŏnja <hardboiled65@gmail.com>
2021-11-15 09:54:53 +0900

    Mobile header bar menu button testing.

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import classNames from 'classnames'
 
@@ -15,28 +15,46 @@ interface HeaderBarProps {
 }
 
 const HeaderBar = (props: HeaderBarProps) => {
+  //============
+  // Hooks
+  //============
   const swingby = useSwingby();
 
+  const [showLinks, setShowLinks] = useState(false);
+
+  function isMobile(): boolean {
+    switch (props.mobileSize) {
+      case 'xs':
+        return swingby.screen.width < swingby.screen.sizes['sm'];
+      case 'sm':
+        return swingby.screen.width < swingby.screen.sizes['md'];
+      case 'md':
+        return swingby.screen.width < swingby.screen.sizes['lg'];
+      case 'lg':
+        return swingby.screen.width < swingby.screen.sizes['xl'];
+      case 'xl':
+        return swingby.screen.width >= swingby.screen.sizes['xl'];
+      default:
+        return false;
+    }
+  }
+
+  //============
+  // Class
+  //============
   const classes = classNames({
     's-header-bar': true,
-    's-header-bar--mobile': (() => {
-      switch (props.mobileSize) {
-        case 'xs':
-          return swingby.screen.width < swingby.screen.sizes['sm'];
-        case 'sm':
-          return swingby.screen.width < swingby.screen.sizes['md'];
-        case 'md':
-          return swingby.screen.width < swingby.screen.sizes['lg'];
-        case 'lg':
-          return swingby.screen.width < swingby.screen.sizes['xl'];
-        case 'xl':
-          return swingby.screen.width >= swingby.screen.sizes['xl'];
-        default:
-          return false;
-      }
-    })(),
+    's-header-bar--mobile': isMobile(),
   });
 
+  const menuButtonClasses = classNames({
+    's-header-bar__menu-button': true,
+    's-header-bar__menu-button--active': showLinks,
+  });
+
+  //============
+  // Style
+  //============
   const styles = {
     height: props.height,
   };
@@ -46,6 +64,14 @@ const HeaderBar = (props: HeaderBarProps) => {
     height: `calc(${props.height} - 16px)`,
   };
 
+  const menuButtonStyles = {
+    width: `calc(${props.height} - 16px)`,
+    height: `calc(${props.height} - 16px)`,
+  };
+
+  //===============
+  // Children
+  //===============
   const headerBarLinks = React.Children.toArray(props.children)
     .filter(child => {
       if (!React.isValidElement(child) || typeof child.type === 'string') {
@@ -58,6 +84,18 @@ const HeaderBar = (props: HeaderBarProps) => {
     <div className={classes}
       style={styles}
     >
+      {isMobile() &&
+        <div
+          className={menuButtonClasses}
+          style={menuButtonStyles}
+          onClick={() => {
+            setShowLinks(!showLinks);
+          }}
+        >
+          <div className="s-header-bar__menu-button__bun-top"></div>
+          <div className="s-header-bar__menu-button__bun-bottom"></div>
+        </div>
+      }
       <div className="s-header-bar__logo">
         <img
           src={props.logo}
