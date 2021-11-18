e5b94e71c24b4d3e31c03a430b172f8a320c25bf
Yujŏnja <hardboiled65@gmail.com>
2021-11-18 09:12:29 +0900

    Change some cli command names.

--- cli/bin/swingby.js
+++ cli/bin/swingby.js
@@ -18,13 +18,13 @@ process.on('unhandledRejection', err => {
 const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
-const scriptIndex = args.findIndex(
-  x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
+const scriptIndex = args.findIndex(x =>
+  x === 'build' || x === 'dev' || x === 'new' || x === 'test'
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
-if (['build', 'eject', 'start', 'test'].includes(script)) {
+if (['build', 'dev', 'new', 'test'].includes(script)) {
   const result = spawn.sync(
     process.execPath,
     nodeArgs
