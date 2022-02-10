8fc724dff5310080a9e48a5bd35af97563f8833a
Yujŏnja <hardboiled65@gmail.com>
2022-02-10 17:35:18 +0900

    Button appearance option.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ import './Button.scss'
 
 interface ButtonProps {
   children: React.ReactNode;
+  appearance: 'rectangle' | 'round' | 'sharp';
   label: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
@@ -13,7 +14,9 @@ interface ButtonProps {
 const Button = (props: ButtonProps) => {
   const classes = classNames({
     's-button': true,
-    's-button--rectangle': true,
+    's-button--rectangle': props.appearance === 'rectangle',
+    's-button--round': props.appearance === 'round',
+    's-button--sharp': props.appearance === 'sharp',
     's-pa-md': true,
   });
 
@@ -29,8 +32,9 @@ const Button = (props: ButtonProps) => {
 
 Button.defaultProps = {
   children: [],
+  appearance: 'rectangle',
   label: 'Button',
   onClick: null,
-};
+} as ButtonProps;
 
 export default Button
